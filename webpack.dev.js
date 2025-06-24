6280e6502b1cadbf529b707f065b9ead15e0f106
Douglas Senzu <senzdigitals.inc23@gmail.com>
2025-06-24 12:31:55 +0000

    Project folder structure setup completed

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./public/index.html"],
+  },
+  plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+  ],
+  module: {
+    rules: [
+        {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+      
+    ]
+  }
+};
