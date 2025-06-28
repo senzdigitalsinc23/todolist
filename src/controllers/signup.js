135e57f8bac73e81d7e2ba1cd7b90b6ad47b03c4
Douglas Senzu <senzdigitals.inc23@gmail.com>
2025-06-28 01:24:12 +0000

    User registration backend completed with 3 factory functions (userDetails to get user details, validateUserDetails to validate user input and registerUser to actually register the user if validations pass

---
+++ src/controllers/signup.js
@@ -0,0 +1,75 @@
+
+
+let userDetails = (username, password, confirmPass) => {
+    
+    const getUsername = () => {
+        return username;
+    }
+
+    const getPassword = () => {
+        return password;
+    }
+
+    const getConfirm = () => {
+        return confirmPass;
+    }
+
+    return { getUsername, getPassword, getConfirm };
+}
+
+let validateUserDetails = () => {
+    
+    const checkUsername = () => { 
+        if(user.getUsername() != "") {
+            return true;
+        }
+        return false;
+    }
+
+    const checkPassword = () => { 
+        if(user.getPassword() != "") {
+            return true;
+        }
+        return false;
+    }
+
+    const passwordMatches = () => { 
+        if(user.getPassword() == user.getConfirm()) {
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        checkUsername,
+        checkPassword,
+        passwordMatches
+    }
+}
+
+let registerUser = () => {
+    let message = '';
+
+    if (validate.checkUsername()) {
+        if (validate.checkPassword()) {
+            if (validate.passwordMatches()) {
+                let userInfo = {
+                    'username': user.getUsername(),
+                    'password': user.getPassword()
+                }
+
+                localStorage.setItem('user', JSON.stringify(userInfo));
+
+                message = "User " + user.getUsername().toUpperCase() + " registered successfully";
+            }else {
+                message =  "Passwords do not match";
+            }
+        }else {
+            message =  "Password is required";
+        }
+    }else {
+        message =  "Username is required";
+    }
+
+    return message;
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./assets/css/reset.css";
 import "./assets/css/styles.css";
+
+import "./controllers/signup.js";
+
